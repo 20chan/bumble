7ef8fe92e5d5caf7dbbb8a6dd70b53e3f32dd6ba
phillyai <sonagi.philly@gmail.com>
2017-03-05 04:43:30 +0900

    parse_number 추가

--- parser.py
+++ parser.py
@@ -9,15 +9,17 @@ def parse(code: str):
             yield tok
 
 
-def parse_token(code, index=0) -> (Token, int):
+def parse_token(code: str, index=0) -> (Token, int):
     current = code[index:]
     if current[0] in '\r\n\t':
         return Token(current[0], TokenType.NONE)
     if current[0] is '"':
         return parse_string(current)
+    if str.isdigit(current[0]):
+        return parse_number(current)
 
 
-def decode_escape(char):
+def decode_escape(char: str) -> str:
     if char == '\\':
         return '\\'
     if char == 'n':
@@ -36,16 +38,34 @@ def decode_escape(char):
     return None
 
 
-def parse_string(code):
+def parse_string(code: str) -> Token:
     res = ''
     i = 1
     while i < len(code) and code[i] != '"':
         if code[i] == '\\':
-            res += decode_escape(code[i+1])
+            decoded = decode_escape(code[i+1])
+            if decoded is None:
+                return Token(None, TokenType.ERROR)
+            res += decoded
             i += 1
         else:
             res += code[i]
         i += 1
-    return res
+    return Token(res, TokenType.STRING)
 
-print(parse_string('"wow \\tthis is \\nawesome" lol'))
+
+def parse_number(code: str) -> Token:
+    i = 0
+    result_type = TokenType.INTEGER
+    while i < len(code):
+        if code[i] == '.':
+            if result_type == TokenType.REAL:
+                return Token(None, TokenType.ERROR)
+            result_type = TokenType.REAL
+        if not str.isdigit(code[i]):
+            return Token(code[:i+1], result_type)
+        i += 1
+
+
+if __name__ == '__main__':
+    print(parse_string(open('code.txt', encoding='utf-8').read()).code)
