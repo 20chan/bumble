c516f76d743a6f8686427310b29213a732c1497d
phillyai <sonagi.philly@gmail.com>
2017-03-07 15:53:28 +0900

    ReadMe 업데이트

--- parse/AST.py
+++ parse/AST.py
@@ -9,6 +9,13 @@ class BinOp(AST):
         self.right = right
 
 
+class Assign(AST):
+    def __init__(self, left, op, right):
+        self.left = left
+        self.token = self.op = op
+        self.right = right
+
+
 class Num(AST):
     def __init__(self, token):
         self.token = token
