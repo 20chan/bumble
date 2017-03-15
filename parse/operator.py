9ed1565b956e5fa23faa1c170a5440236d8e10ca
phillyai <sonagi.philly@gmail.com>
2017-03-15 15:48:32 +0900

    연산자 우선순위 추가

---
+++ parse/operator.py
@@ -0,0 +1,37 @@
+from enum import Enum
+
+
+class Direction(Enum):
+    NONE = 0
+    LEFT = 1
+    RIGHT = 2
+
+precedences = {
+    '=': (1, Direction.RIGHT),
+    ':=': (1, Direction.RIGHT),
+    '|>': (2, Direction.LEFT),
+    '=>': (3, Direction.RIGHT),  # => 요것도 연산자 우선순위를 따지나?
+    '.': (2, Direction.LEFT),
+    '()': (2, Direction.LEFT),
+    '||': (3, Direction.LEFT),
+    '&&': (4, Direction.RIGHT),
+    '^': (5, Direction.LEFT),
+    '<<': (6, Direction.LEFT),
+    '>>': (6, Direction.LEFT),
+    '<': (7, Direction.NONE),
+    '<=': (7, Direction.NONE),
+    '>': (7, Direction.NONE),
+    '>=': (7, Direction.NONE),
+    '==': (7, Direction.NONE),
+    '!=': (7, Direction.NONE),
+    ':': (8, Direction.RIGHT),
+    '++': (8, Direction.RIGHT),
+    '+': (9, Direction.LEFT),
+    '-': (9, Direction.LEFT),
+    '*': (10, Direction.LEFT),
+    '/': (10, Direction.LEFT),
+    '%': (10, Direction.LEFT),
+    '**': (11, Direction.RIGHT)
+}
+
+
