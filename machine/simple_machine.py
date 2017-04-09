64fbac531d09346cc80bf4adff77290970e1854d
phillyai <sonagi.philly@gmail.com>
2017-04-09 22:11:27 +0900

    클래스-객체 프로토타입

--- machine/simple_machine.py
+++ machine/simple_machine.py
@@ -4,5 +4,18 @@ from parse.AST import Node
 from typing import List, Dict
 
 
+class Class:
+    def __init__(self, parent, sentences: List[Node.Sentence]):
+        self.parent = parent
+        self.sentences = sentences
+
+    def make_instance(self) -> Object:
+        pass
+
+
+class Object:
+    pass
+
+
 class SimpleMachine(Machine):
     pass
