5580e086a0de0d5a799467e27891f497423c8304
phillyai <sonagi.philly@gmail.com>
2017-04-12 15:04:24 +0900

    Symbol, SymbolTable 정의

--- machine/simple_machine.py
+++ machine/simple_machine.py
@@ -4,17 +4,35 @@ from parse.AST import Node
 from typing import List, Dict
 
 
-class Class:
-    def __init__(self, parent, sentences: List[Node.Sentence]):
-        self.parent = parent
-        self.sentences = sentences
-
-    def make_instance(self) -> Object:
-        pass
+class Symbol:
+    def __init__(self, name, typ=None):
+        self.name = name
+        self.type = typ
 
 
-class Object:
-    pass
+class VarSymbol(Symbol):
+    def __init__(self, name, typ):
+        Symbol.__init__(self, name, typ)
+
+
+class BuiltinTypeSymbol(Symbol):
+    def __init__(self, name):
+        Symbol.__init__(self, name)
+
+
+class SymbolTable:
+    def __init__(self):
+        from collections import OrderedDict
+        self._symbols = OrderedDict()
+        self._init_builtins()
+
+    def _init_builtins(self):
+        types = ['INTEGER', 'REAL', 'STRING', 'BOOL']
+        for typ in types:
+            self.define(BuiltinTypeSymbol(typ))
+
+    def define(self, symbol):
+        self._symbols[symbol.name] = symbol
 
 
 class SimpleMachine(Machine):
