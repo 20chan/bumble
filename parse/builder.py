05adc62ac63b9fb3e825dbb03d344ec974260ddb
phillyai <sonagi.philly@gmail.com>
2017-04-24 23:01:15 +0900

    파서 완성

--- parse/builder.py
+++ parse/builder.py
@@ -14,6 +14,10 @@ class Builder:
     def top(self):
         return self._toks[0]
 
+    @property
+    def empty(self):
+        return len(self._toks) <= 1
+
     def check_pop(self, tok):
         assert(self.pop().code == tok)
 
@@ -27,16 +31,42 @@ class Builder:
     def parse_statement(self) -> Node.Node:
         l = self.parse_function()
         if self.top.type == TokenType.EQUAL:
+            self.check_pop('=')
             r = self.parse_value()
+            if not self.empty:
+                self.check_pop(',')
             return Node.AssignNode(l, r)
+        if not self.empty:
+            self.check_pop(',')
         return l
 
     def parse_value(self) -> Node.ValueNode:
         if self.top.is_literal():
             return self.parse_literal()
+        return self.parse_function()
 
     def parse_function(self) -> Node.FunctionNode:
-        pass
+        res = []
+        while not self.empty \
+                and self.top.type not in [TokenType.COMMA, TokenType.EQUAL, TokenType.RBRAKET]:
+            if self.top.type == TokenType.LBRAKET:
+                self.check_pop('(')
+                res.append(self.parse_value())
+                self.check_pop(')')
+            elif self.top.is_literal():
+                res.append(self.parse_literal())
+            else:
+                res.append(Node.Identifier(self.pop().code))
+        return Node.FunctionNode(res)
 
     def parse_literal(self) -> Node.Literal:
         return Node.Literal(self.pop())
+
+
+def main():
+    b = Builder(open('code.txt', encoding='utf-8').read())
+    res = b.parse()
+    print('parsed!')
+
+if __name__ == '__main__':
+    main()
